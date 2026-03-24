26d228e6ae8728d3b6e6cdffd173aea7991bb2d3
sumit <sumitlodhi@9401>
2026-03-24 12:22:58 +0530

    ain

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className='py-7 bg-dark-300'>
         <div className='container text-center mx-auto px-6'>
-            <p>© 2026 Created By Sumit Rajput. All rights reserved.</p>
+            <p>© 2028 Created By Sumit Rajput. All rights reserved.</p>
         </div>
     </div>
   )
